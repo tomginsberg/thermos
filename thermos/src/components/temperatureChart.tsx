18fa2111de57e088fe9144a3e51b438458b68381
Tom Ginsberg <tom@beit.tech>
2025-01-15 16:52:53 -0500

    V2 application

--- thermos/src/components/temperatureChart.tsx
+++ thermos/src/components/temperatureChart.tsx
@@ -1,17 +1,15 @@
 "use client"
 
 import {useState, useEffect} from 'react'
-import {CartesianGrid, Line, LineChart, XAxis, YAxis, ResponsiveContainer, Tooltip} from "recharts"
+import {CartesianGrid, Line, LineChart, XAxis, YAxis, Tooltip} from "recharts"
 import {format, parseISO} from 'date-fns'
 
 import {
     ChartConfig,
     ChartContainer,
-    ChartTooltip,
-    ChartTooltipContent,
 } from "@/components/ui/chart"
-import {processChartData, generateXAxisTicks} from '@/lib/processChartData'
-import {Card, CardContent} from "@/components/ui/card.tsx";
+import {processChartData, generateXAxisTicks, generateYAxisTicks} from '@/lib/processChartData'
+import {Skeleton} from "@/components/ui/skeleton";
 
 const chartConfig = {
     temperature: {
@@ -24,17 +22,18 @@ const chartConfig = {
     },
 } satisfies ChartConfig
 
-function formatHourAmPm(date) {
+function formatHourAmPm(date: Date) {
     let hours = date.getHours(); // Get the hour (0-23)
     const amPm = hours >= 12 ? 'pm' : 'am'; // Determine am or pm
     hours = hours % 12 || 12; // Convert 0 (midnight) and 12 (noon) to 12
-    return `${hours}${amPm}`; // Concatenate the formatted hour and am/pm
+    return `${hours}:${date.getMinutes()}${amPm}`; // Concatenate the formatted hour and am/pm
 }
 
 
 export function TemperatureChart({downsampleRate = 10}) {
     const [chartData, setChartData] = useState([])
     const [xAxisTicks, setXAxisTicks] = useState([])
+    const [yAxisTicks, setYAxisTicks] = useState([])
     const [isLoading, setIsLoading] = useState(true);
     const [error, setError] = useState<string | null>(null);
 
@@ -49,7 +48,6 @@ export function TemperatureChart({downsampleRate = 10}) {
                 return response.json();
             })
             .then(data => {
-                console.log('Raw data:', data);
                 if (!data || !Array.isArray(data.temperature) || !Array.isArray(data.setpoint) || !Array.isArray(data.time)) {
                     throw new Error('Data is not in the expected format');
                 }
@@ -59,10 +57,13 @@ export function TemperatureChart({downsampleRate = 10}) {
                     setpoint: data.setpoint[index]
                 }));
                 const processedData = processChartData(combinedData, downsampleRate);
-                console.log('Processed data:', processedData);
-                setXAxisTicks(generateXAxisTicks(processedData));
-                setChartData(processedData);
-                setIsLoading(false);
+                // @ts-expect-error - processedData is an array of objects
+                setXAxisTicks(generateXAxisTicks(processedData))
+                // @ts-expect-error - processedData is an array of objects
+                setYAxisTicks(generateYAxisTicks(processedData))
+                // @ts-expect-error - processedData is an array of objects
+                setChartData(processedData)
+                setIsLoading(false)
             })
             .catch(error => {
                 console.error('Error fetching or processing data:', error);
@@ -71,10 +72,10 @@ export function TemperatureChart({downsampleRate = 10}) {
                 setError(error.message);
                 setIsLoading(false);
             });
-    }, [])
+    }, [downsampleRate])
 
     if (isLoading) {
-        return <div>Loading...</div>;
+        return <Skeleton className="h-[384px] bg-zinc-200 opacity-20 mx-8 mb-4"/>;
     }
 
     if (error) {
@@ -83,20 +84,21 @@ export function TemperatureChart({downsampleRate = 10}) {
 
     return (
         <ChartContainer config={chartConfig} className="h-[400px] ">
-            <ResponsiveContainer width="100%" height="100%">
                 <LineChart
                     data={chartData}
                     margin={{
                         top: 5,
                         right: 30,
                         left: 10,
-                        bottom: 5,
+                        bottom: 20,
                     }}
                 >
                     <CartesianGrid strokeDasharray="3 3"/>
                     <XAxis
                         dataKey="time" // the ISO string
                         tickFormatter={(value: string) => {
+                            // if (value===xAxisTicks[xAxisTicks.length - 1]) {
+                            //     return 'now'}
                             // parse the ISO string and display "HH:mm"
                             const date = parseISO(value);
                             return formatHourAmPm(date);
@@ -107,10 +109,10 @@ export function TemperatureChart({downsampleRate = 10}) {
                     />
                     <YAxis
                         domain={['dataMin - 0.5', 'dataMax + 0.5']}
-                        tickCount={5}
+                        ticks={yAxisTicks}
                         tick={{stroke: 'white'}}
                         tickFormatter={
-                            (value: number) => `${value.toFixed(1)}°C`
+                            (value: number) => `${value}°C`
                         }
                     />
                     <Tooltip
@@ -137,7 +139,6 @@ export function TemperatureChart({downsampleRate = 10}) {
                         dot={false}
                     />
                 </LineChart>
-            </ResponsiveContainer>
         </ChartContainer>
     )
 }
