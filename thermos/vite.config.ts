66e8e1b1827a1b47c92dbb83256b2dd1b224f34c
Tom Ginsberg <tom@beit.tech>
2025-01-15 17:15:05 -0500

    pwa

--- thermos/vite.config.ts
+++ thermos/vite.config.ts
@@ -1,9 +1,26 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
+import { VitePWA } from 'vite-plugin-pwa'
+// Import JSON file
+import iconsData from './icons.json'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Heat',
+        short_name: 'Heat',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#000000',
+        theme_color: '#000000',
+        // use the icons from iconsData
+        icons: iconsData.icons
+      }
+    })
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
